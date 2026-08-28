736196f0439ab9cba6e20e1cb0a8ae3cc5bdc77c
m4rm0t <esteban.madrazo-bb@proton.me>
2026-08-28 11:11:17 +0200

    chore : Initialisation du projet)

---
+++ p_oo-parking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace p_oo_parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
